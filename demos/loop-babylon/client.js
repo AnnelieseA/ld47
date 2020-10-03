4da96cebc556206e00f1221d14b255b276102b9c
Derrick Schwabe <admin@nerdbusiness.com>
2020-10-02 23:35:22 -0600

    Complete basic rotation animation setup for orbs deal

--- demos/loop-babylon/client.js
+++ demos/loop-babylon/client.js
@@ -12,12 +12,19 @@ $('body').append(`
   </canvas>
 `)
 
+//little math helper:
+const degreesToRadians = degrees => {
+  var pi = Math.PI
+  return degrees * (pi/180)
+}
+
 import * as BABYLON from 'babylonjs'
 
 // Get the canvas DOM element
 const canvas = document.getElementById('renderCanvas');
 // Load the 3D engine
 const engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true})
+
 // CreateScene function that creates and return the scene
 const createScene = () => {
   // Create a basic BJS Scene object
@@ -25,7 +32,7 @@ const createScene = () => {
   scene.clearColor = BABYLON.Color3.Gray()
 
   // Create a camera, and set its position to {x: 0, y: 5, z: -10}
-  let camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, -10), scene)
+  let camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, -20), scene)
 
   // Target the camera to scene origin
   camera.setTarget(BABYLON.Vector3.Zero())
@@ -34,11 +41,12 @@ const createScene = () => {
   camera.attachControl(canvas, false) //< UP/DOWN/LEFT/RIGHT keyboard controls...
   //^ also mouse controlling
 
-  // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
+  // Create a basic light
   let light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene)
-  // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
+
+  // Create a built-in "sphere" shape
   let sphere = BABYLON.Mesh.CreateSphere('sphere', 16, 2.5, scene, false, BABYLON.Mesh.FRONTSIDE)
-  // Move the sphere
+  // Move sphere into position:
   sphere.position.y = -2
   sphere.position.x = -2
 
@@ -46,16 +54,10 @@ const createScene = () => {
   sphere2.position.y = 2
   sphere2.position.x = 2
 
-  let bezierTorus = BABYLON.Mesh.CreateTorus("torus", 6 , 0.2, 32, scene, false, BABYLON.Mesh.FRONTSIDE)
+  let bezierTorus = BABYLON.Mesh.CreateTorus("torus", 6 , 0.2, 64, scene, false, BABYLON.Mesh.FRONTSIDE)
   bezierTorus.position.x = 0
   bezierTorus.position.z = 0
   bezierTorus.rotation.x = Math.PI / 2
-
-  //^ same as calling:
-  //bezierTorus.rotate(BABYLON.Axis.X,  Math.PI / 2  , BABYLON.Space.WORLD)
-
-  //# TIP # degrees conversion:
-  // 45 == Math.PI / 4, 90 == Math.PI / 2 and 180 == Math.PI ;)
 
   let blackMaterial = new BABYLON.StandardMaterial("texture1", scene)
   blackMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0)
@@ -67,8 +69,21 @@ const createScene = () => {
   sphere.material = blackMaterial
   sphere2.material = whiteMaterial
 
-  // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-  //var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
+  //create parent object ('midNode') to use as a sort of origin point for the basis of rotation (or movement):
+  let midNode = new BABYLON.TransformNode("midNode")
+
+  //set the other objects to be children:
+  sphere.parent = midNode
+  sphere2.parent = midNode
+  bezierTorus.parent = midNode
+
+  //rotation animation:
+  let degrees = 0
+  scene.registerAfterRender( () => {
+    midNode.rotation.z = degreesToRadians(degrees + 1)
+    degrees = degrees + 1
+  })
+
   // Return the created scene
   return scene
 }
