28dcff6aabcc239ecd8fe8a0d2d30e75d348fbd4
Derrick Schwabe <admin@nerdbusiness.com>
2020-10-03 20:00:20 -0600

    Refactor loop instantiation to make it easier to insert multiple instances, control speeds independently for each loop.

--- demos/rotate-loop-2-loops/client.js
+++ demos/rotate-loop-2-loops/client.js
@@ -4,45 +4,55 @@ global.$ = require('jquery')
 
 $('body').addClass('bg-silver')
 
-let loopContainerElement = `
+// import html from lit-html
+
+let loopContainerElementInstance = (id, top, left, size) => $(`
   <div class="LOOPCONTAINER absolute"
-       style="top: 200px; left: 200px; width: 400px; height: 400px;
+       style="top: ${top}px; left: ${left}px; width: ${size}px; height: ${size}px;
              transform-origin: center center;
-     ">
+     "
+     id = "LOOPCONTAINER-${id}"
+     >
     <div class="INNER relative"></div>
 </div>
-`
-$(`body`).append(loopContainerElement)
+`)
 
-let botElement = `
+let $loopContainerElementA = loopContainerElementInstance('A', 200, 200, 400)
+$loopContainerElementA.appendTo('body')
+
+let botElementInstance = (top, left, color) => $(`
   <div class="BOT absolute inline-block z2" 
-     style="border-radius: 100%; width:60px; height: 60px; background-color: deeppink;
-     top: 160px;
-     left: -25px;
+     style="border-radius: 100%; width:60px; height: 60px; background-color: ${color};
+     top: ${top}px;
+     left: ${left}px;
      ">
       &nbsp;
   </div>
-`
-
-$('.LOOPCONTAINER > .INNER').append(botElement)
+`)
+$loopContainerElementA.first().append(botElementInstance(160, -25, 'deeppink'))
 
 //LOOP tether:
-let loopTetherElement = `
+let loopTetherElementInstance = (size, color) => $(`
   <div class="LOOPTETHER absolute inline-block p4 border-2 z1" 
-     style="border-radius: 100%; width:400px; height: 400px; border-color: deeppink;
+     style="border-radius: 100%; width:${size}px; height: ${size}px; border-color: ${color};
       top: 0px;
       left: 0px;
      ">
       &nbsp;
   </div>
-`
+`)
+$loopContainerElementA.first().append( loopTetherElementInstance(400, 'deeppink') )
 
-$('.LOOPCONTAINER > .INNER').append(loopTetherElement)
+//second loop:
+let $loopContainerElementB = loopContainerElementInstance('B', 224, 240, 200, 'black')
+$loopContainerElementB.appendTo('body')
+$loopContainerElementB.first().append(botElementInstance(120, -5, 'black'))
+$loopContainerElementB.first().append( loopTetherElementInstance(200, 'black') )
 
 //rotate animation:
 const anime = require('animejs')
-const animate = () => anime({
-  targets:  '.LOOPCONTAINER',
+const animate = id => anime({
+  targets:  '#' + id,
   rotateZ : 360,
   easing: 'linear',
   autoplay : false,
@@ -51,10 +61,10 @@ const animate = () => anime({
 
 //handle input; advance/brake speed on keydown, reset back to idle speed on keyup:
 $(document).on('keydown', e => {
-  if(e.code === 'ArrowLeft') timeManager.updateSpeed(0.08)
-  if(e.code === 'ArrowRight') timeManager.updateSpeed(0.8)
+  if(e.code === 'ArrowLeft') timeManagerLoopA.updateSpeed(0.08)
+  if(e.code === 'ArrowRight') timeManagerLoopA.updateSpeed(0.8)
 })
-$(document).on('keyup', () => timeManager.updateSpeed(idleSpeed))
+$(document).on('keyup', () => timeManagerLoopA.updateSpeed(idleSpeed))
 
 class TimeMagic {
   constructor(speed) {
@@ -78,17 +88,21 @@ class TimeMagic {
     return this.accumulateTime
   }
 }
-const timeManager = new TimeMagic(1)
+const timeManagerLoopA = new TimeMagic(1)
+const timeManagerLoopB = new TimeMagic(1)
 
 //set speed and begin animation loop:
 const idleSpeed = 0.25
-timeManager.updateSpeed(idleSpeed)
+timeManagerLoopA.updateSpeed(idleSpeed)
+timeManagerLoopB.updateSpeed(idleSpeed)
 
-let animation = animate()
-
+let animation = animate($loopContainerElementA.attr('id'))
+let animationB = animate($loopContainerElementB.attr('id'))
 function loop(t) {
-  timeManager.updateTime(t)
-  animation.tick(timeManager.getTime())
+  timeManagerLoopA.updateTime(t)
+  timeManagerLoopB.updateTime(t)
+  animation.tick(timeManagerLoopA.getTime())
+  animationB.tick(timeManagerLoopB.getTime())
   customRAF = requestAnimationFrame(loop)
 }
-//requestAnimationFrame(loop)
+requestAnimationFrame(loop)
